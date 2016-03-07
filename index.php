f43be2e5c6ef16b26e78167add038cf717b6a0f0
minsau <minsau2@gmail.com>
2016-03-07 16:19:11 -0600

    Creacion repositorio

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<link rel="stylesheet" href="includes/css/style.css">
+	<title> Blog | Un Ingeniero en Sistemas	</title>
+	</head>
+	<body>
+		<div name="content" id="content">
+			<h2> Bienvenido ->User<- </h2>
+			<h4> Para comentar alguno de los posts, debes estar registrado. Puedes registrarte <a href="login.php"> aqui. </a>
+			
+			<div name="entradas-container" id="entradas-container">
+				<div class="entries" name="entrada1" id="entrada1">
+					<div id="title" onclick="javascript:parent.location='entrada1.php'"><h3> Titulo de entrada 1 </h3> </div>
+					<div id="body"> <h4>Aqui va el contenido del postlskfsdvnslvnslvlsdnvlsdvnlsvlsdnvlsdvlsdnvslvnldsnvlsdv </h4></div>
+				</div>
+
+				<div class="entries" name="entrada1" id="entrada1">
+               <div id="title" onclick="javascript:parent.location='entrada2.php'"><h3> Titulo de entrada 2 </h3> </div>
+               <div id="body"> <h4>Aqui va el contenido del postlskfsdvnslvnslvlsdnvlsdvnlsvlsdnvlsdvlsdnvslvnldsnvlsdv </h4></div>
+            </div> 
+		
+			</div>
+
+		</div>
+	</body>
+</html>
