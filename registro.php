41516197f5cdad28b5e391fe61c348f9ff4920c5
efrencruz <efrenodiloncruzcruz@hotmail.com>
2016-03-07 23:48:44 -0600

    .

--- registro.php
+++ registro.php
@@ -17,12 +17,12 @@
 			<h3 align="center" name="usuario-container" id="usuario-container"><font color="white"> Ingresa tus datos para registrarte</font></h3>
 			<br>
 			<div class="registro-container">
-			<label for="nombre">Nombre: </label><input type="text" name="nombre" id="nombre" placeholder="Ingresa tu nombre"><br>
-			<label for="apellidos">Apellidos: </label><input type="text" name="apellidos" id="apellidos" placeholder="Ingresa tus apellidos"><br>
-			<label for="alias">Alias: </label><input type="alias" name="alias" id="alias" placeholder="Ingresa un alias"><br>
-			<label for="correo">Correo: </label><input type="email" name="correo" id="correo" placeholder="Ingresa tu correo"><br>
-			<label for="fecha_nacimiento">Fecha de nacimiento: </label><input type="date" name="fecha_nacimiento" id="fecha_nacimiento" placeholder="Año-Mes-Dia"><br>
-			<label for="pass">Contraseña: </label> <input type="password" name="pass" id="pass" placeholder="Ingresa tu contraseña">
+			<label for="nombre">Nombre: </label><input type="text" name="nombre" id="nombre" placeholder="Ingresa tu nombre" required><br>
+			<label for="apellidos">Apellidos: </label><input type="text" name="apellidos" id="apellidos" placeholder="Ingresa tus apellidos" required><br>
+			<label for="alias">Alias: </label><input type="alias" name="alias" id="alias" placeholder="Ingresa un alias" required><br>
+			<label for="correo">Correo: </label><input type="email" name="correo" id="correo" placeholder="Ingresa tu correo" required><br>
+			<label for="fecha_nacimiento">Fecha de nacimiento: </label><input type="date" name="fecha_nacimiento" id="fecha_nacimiento" placeholder="Año-Mes-Dia" required><br>
+			<label for="pass">Contraseña: </label> <input type="password" name="pass" id="pass" placeholder="Ingresa tu contraseña" required>
 
 			<input type="submit" class="btn btn-primary" value="Registrarse">
 			</div>
@@ -44,7 +44,17 @@
 	$correo = $_POST['correo'];
 	$fecha_nacimiento = $_POST['fecha_nacimiento'];
 	$password = $_POST['pass'];
-	$sql = "insert into Usuario values (null,'$nombre','$apellidos','$alias','$correo',md5('$pass'),'Usuario','$fecha_nacimiento')";
+	$sql = "insert into Usuario values (null,'$nombre','$apellidos','$alias','$correo',md5('$pass'),'usuario','$fecha_nacimiento')";
 	$res = mysql_query($sql,$con) or die("Error al registrarse"); 	
+	$sql = "Select * from Usuario where correo = '$correo'";
+	$res = mysql_query($sql, $con);
+	$reg = mysql_fetch_array($res);
+	$_SESSION['id'] = $reg['id_usuario'];
+	echo "<script language='javascript'>
+			alert('Fuiste Registrado');
+			</script>";
+	if($reg['tipo'] = 'usuario'){
+			header("Location: index.php");
+		}
 	}
 ?>
