c4ea6f35a1a0d0eaeb8c06fd64c094a37651616b
efrencruz <efrenodiloncruzcruz@hotmail.com>
2016-03-07 20:08:12 -0600

    Meti conexion a includes

---
+++ includes/conexion.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost";
+$database = "bdBlog";
+$username = "root";
+$password = "uporeme13.,.";
+$con = mysql_connect($host, $username, $password) or die ("ERROR de conexion");
+$db_selected = mysql_select_db($database, $con) or die ("ERROR");
+?>
