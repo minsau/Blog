d01eb1779d9f4605b6ea2f189d2f791b093f18f3
efrencruz <efrenodiloncruzcruz@hotmail.com>
2016-03-07 19:57:20 -0600

    Se ven las publicaciones actuales

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+require_once("conexion.php");
+$sql_pub = "select * from Blog as b, Publicacion as p where p.id_blog = b.id_blog order by fecha_publicacion desc";
+$res_pub = mysql_query($sql_pub, $con);
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -6,18 +12,20 @@
 	</head>
 	<body>
 		<div name="content" id="content">
-			<h2> Bienvenido ->User<- </h2>
-			<h4> Para comentar alguno de los posts, debes estar registrado. Puedes registrarte <a href="login.php"> aqui. </a>
+			<h2 align="center"> Bienvenido</h2>
+			<h4 align="center"> Para comentar alguno de los posts, debes iniciar sesión <a href="login.php"> aqui </a> </h4>
 			
 			<div name="entradas-container" id="entradas-container">
 				<div class="entries" name="entrada1" id="entrada1">
-					<div id="title" onclick="javascript:parent.location='entrada1.php'"><h3> Titulo de entrada 1 </h3> </div>
-					<div id="body"> <h4>Aqui va el contenido del postlskfsdvnslvnslvlsdnvlsdvnlsvlsdnvlsdvlsdnvslvnldsnvlsdv </h4></div>
+					<?php
+					while($reg_pub = mysql_fetch_array($res_pub)){
+					?>
+					<div id="title" onclick="javascript:parent.location='entrada1.php'"><h3> <?php echo $reg_pub['tema']." - ".$reg_pub['titulo']." - ".$reg_pub['fecha_publicacion']; ?> </h3> </div>
+					<div id="body"> <h4> <?php echo $reg_pub['contenido']; ?> </h4></div>
+					<?php
+					}
+					?>
 				</div>
-
-				<div class="entries" name="entrada1" id="entrada1">
-               <div id="title" onclick="javascript:parent.location='entrada2.php'"><h3> Titulo de entrada 2 </h3> </div>
-               <div id="body"> <h4>Aqui va el contenido del postlskfsdvnslvnslvlsdnvlsdvnlsvlsdnvlsdvlsdnvslvnldsnvlsdv </h4></div>
             </div> 
 		
 			</div>
