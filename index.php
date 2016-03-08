a248b2fcfb75b7981a9725d32cf8b40ae50ab8bd
efrencruz <efrenodiloncruzcruz@hotmail.com>
2016-03-08 08:51:47 -0600

    Cambios

--- index.php
+++ index.php
@@ -7,11 +7,14 @@ $sql_usuario = "select * from Usuario where id_usuario = $id_usuario";
 $res_usuario = mysql_query($sql_usuario, $con);
 $reg_usuario = mysql_fetch_array($res_usuario);
 
-$sql_pub = "select * from Blog as b, Publicacion as p where p.id_blog = b.id_blog order by fecha_publicacion desc";
+$sql_pub = "select * from Blog as b, Publicacion as p where p.id_blog = b.id_blog order by fecha_publicacion desc"; 
 $res_pub = mysql_query($sql_pub, $con);
 
 $sql_blog = "select * from Usuario as u, Blog as b where u.id_usuario = b.id_usuario";
 $res_blog = mysql_query($sql_blog, $con);
+
+$sql_pub_u = "select * from Blog as b, Publicacion as p where p.id_blog = b.id_blog and order by fecha_publicacion desc"; 
+$res_pub_u = mysql_query($sql_pub_u, $con);
 
 ?>
 
@@ -32,20 +35,36 @@ $res_blog = mysql_query($sql_blog, $con);
 				</font>
 			</div>	
 
+
+<a href="#miModal">Abrir Modal</a>
+<div id="miModal" class="modal">
+  <div class="modal-contenido">
+    <a href="#">X</a>
+    <form action="#" id="nuevo_blog" method="post">
+			<h3 align="center" name="usuario-container" id="usuario-container"><font color="white"> Nuevo Blog</font></h3>
+			<br>
+			<div class="registro-container">
+			<label for="titulo">Titulo: </label><input type="text" name="titulo" id="titulo" placeholder="Ingresa un titulo" required><br>
+			<label for="descripcion">Descripcion: </label><input type="text" name="apellidos" id="apellidos" placeholder="Ingresa una descripción del blog" required><br>
+			<input type="submit" class="btn btn-primary" value="Guardar blog">
+			</div>
+		</form>
+  </div>  
+</div>
+
 			<div align="left">
 				<h3>Mis Blogs</h3>
-				<a href="blog.php"><label>[+] Agregar blog</label></a>
+				<a href=""><label>[+] Agregar blog</label></a>
 				<br>
 				<?php
 					while($reg_blog = mysql_fetch_array($res_blog)){
 					?>
-					<div id="title" > <h4> <?php echo $reg_blog['titulo']; ?></h4> </div>
-					<br>
+					<label> <a href=""> <?php echo $reg_blog['titulo']; ?> </a> </label>
 					<?php
 					}
 					?>
 			</div>		
-			
+
 			<div name="entradas-container" id="entradas-container">
 				<div class="entries" name="entrada1" id="entrada1">
 					<a href="entradas.php"><label>[+] Agregar publicación</label></a>
@@ -64,10 +83,7 @@ $res_blog = mysql_query($sql_blog, $con);
 					}
 					?>
 				</div>
-            </div> 
-		
-			</div>
 
-		</div>
+   		</div>
 	</body>
 </html>
